2e54c37ae8a99e866980204393730c29a05fa9b6
kiya kebe <kiyakebe799@gmail.com>
2024-08-27 18:17:09 +0300

    finished: project

--- src/components/common/footer/CopyRight.tsx
+++ src/components/common/footer/CopyRight.tsx
@@ -1,13 +1,14 @@
-import { X, Youtube } from "lucide-react"
+import { Instagram, Linkedin, X, Youtube } from "lucide-react"
 
 const CopyRight = () => {
   return (
-    <div className="flex justify-between border-t-2 py-4 md:px-16 lg:px-32">
+    <div className="flex justify-between border-t-2 py-8 md:px-16 lg:px-32 mt-6">
         <p> &copy; 2020 Africa to Silicon Valley, Inc. All Rights Reserved. </p>
         <div className="flex space-x-4">
             <X />
             <Youtube /> 
-
+            <Linkedin />
+            <Instagram />
         </div>
     </div>
   )
